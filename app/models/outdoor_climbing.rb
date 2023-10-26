8e5622015dcfe0692bd7afae1e09c1c0f338db60
SoliriusAdmin129 <soliriusadmin129@Soliriun129sMBP.localdomain>
2023-10-26 16:52:25 +0100

    fix logged entry and navbar link

--- app/models/outdoor_climbing.rb
+++ app/models/outdoor_climbing.rb
@@ -1,10 +1,11 @@
 class OutdoorClimbing < ApplicationRecord
-    belongs_to :user
+    
+    # belongs_to :user
     devise :authenticatable
     has_many :individual_outdoor_climbs
     accepts_nested_attributes_for :individual_outdoor_climbs,
                                     allow_destroy: true,
-                                    reject_if: proc { |att| att['climb_name'].blank? || ['grade'].blank? || ['climbing_style'].blank? || ['tick'].blank? }
+                                    reject_if: proc { |att| att['climb_name'].blank? || ['grade'].blank?}
      
 end
 
