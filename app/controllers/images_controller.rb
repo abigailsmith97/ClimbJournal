3b5f4cb3e257237f00f5ef8d1da98edef98e6e23
SoliriusAdmin129 <soliriusadmin129@Soliriun129sMBP.localdomain>
2023-11-01 11:25:44 +0000

    in process 3:  view image bug fix

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -22,39 +22,19 @@ class ImagesController < ApplicationController
     def show
       # @log_climbs = OutdoorClimbing.where(user_id: current_user.id).order(date: :desc)
         
-    #   @image = Image.find(params[:id])
+      # @image = Image.find(params[:id])
+      
       @images = Dir.glob("public/uploads/")
       
     end
   
 
-    def display
-      @image_folder_path = Rails.root.join('public', 'uploads')
-      @images = Dir.glob(File.join(@image_folder_path, '**', '*.{jpg,png,gif}'))
+    # def display
+    #   @image_folder_path = Rails.root.join('public', 'uploads')
+    #   @images = Dir.glob(File.join(@image_folder_path, '**', '*.{jpg,png,gif}'))
   
-        # render inline: <<-ERB
-        #   <h1>Display Images</h1>
-
-        #   <style>
-        #     .image-grid {
-        #       display: grid;
-        #       grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
-        #       grid-gap: 10px;
-        #     }
-
-        #     .image-grid img {
-        #       max-width: 100%;
-        #       height: auto;
-        #     }
-        #   </style>
-
-        #   <div class="image-grid">
-        #     <% @images.each do |image_path| %>
-        #       <%= image_tag image_path.gsub("#{Rails.root}/public", '') %>
-        #     <% end %>
-        #   </div>
-        # ERB
-      end
+  
+    #   end
 
 
     private
