62bd772c7527d8c4be6d7a8217769262358f7e20
SoliriusAdmin129 <soliriusadmin129@Soliriun129sMBP.localdomain>
2023-11-01 11:07:57 +0000

    in process 2:  view image bug fix

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -13,24 +13,47 @@ class ImagesController < ApplicationController
       @log_climbs = OutdoorClimbing.where(user_id: current_user.id).order(date: :desc)
 
       if @image.save
-        # notice: "Image uploaded successfully."
+        redirect_to root_path, notice: "Your image was successfully uploaded."
       else
         render 'index'
       end
     end
   
     def show
-      @log_climbs = OutdoorClimbing.where(user_id: current_user.id).order(date: :desc)
+      # @log_climbs = OutdoorClimbing.where(user_id: current_user.id).order(date: :desc)
         
     #   @image = Image.find(params[:id])
-      @images = Image.all 
+      @images = Dir.glob("public/uploads/")
       
     end
   
 
-    def display_images
-        @image_folder_path = Rails.root.join('public', 'images')
-        @images = Dir.glob(File.join(@image_folder_path, '*'))
+    def display
+      @image_folder_path = Rails.root.join('public', 'uploads')
+      @images = Dir.glob(File.join(@image_folder_path, '**', '*.{jpg,png,gif}'))
+  
+        # render inline: <<-ERB
+        #   <h1>Display Images</h1>
+
+        #   <style>
+        #     .image-grid {
+        #       display: grid;
+        #       grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+        #       grid-gap: 10px;
+        #     }
+
+        #     .image-grid img {
+        #       max-width: 100%;
+        #       height: auto;
+        #     }
+        #   </style>
+
+        #   <div class="image-grid">
+        #     <% @images.each do |image_path| %>
+        #       <%= image_tag image_path.gsub("#{Rails.root}/public", '') %>
+        #     <% end %>
+        #   </div>
+        # ERB
       end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,7 @@ Rails.application.routes.draw do
 
   resources :individual_outdoor_climbs, only: [:index, :new, :create, :update, :destroy]
 
-  resources :images, only: [:index, :new, :show, :create, :destroy]
+  resources :images, only: [:index, :new, :show, :create, :destroy, :display]
 
 
 end
