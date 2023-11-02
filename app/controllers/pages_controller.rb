b380e974d0660b8be0458f31b21e58b6ecbe038d
SoliriusAdmin129 <soliriusadmin129@SoliriusAdmin129s-MacBook-Pro.local>
2023-11-02 13:17:45 +0000

    image view:homepage shows 3 logs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,10 @@
 class PagesController < ApplicationController
   def index
-    @log_climbs = OutdoorClimbing.all # Fetch all log climbs (you may need to adjust this query)
+    @log_climbs = OutdoorClimbing.order(date: :desc)
   end
 
   def gallery
-    @log_climbs = OutdoorClimbing.all # Adjust this query to fetch the log climbs you want to display
+    @log_climbs = OutdoorClimbing.order(date: :desc)
   end
 
 end
