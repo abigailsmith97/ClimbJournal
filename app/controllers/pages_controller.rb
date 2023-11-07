915a5472b1b94e2afaecaa8169e7f7bc2d44c863
SoliriusAdmin129 <soliriusadmin129@SoliriusAdmin129s-MacBook-Pro.local>
2023-11-07 23:15:25 +0000

    hard grade and filter

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,9 +13,31 @@ class PagesController < ApplicationController
   end
 
   def analytics
+    @individual_outdoor_climbs = IndividualOutdoorClimb.all
     @log_climbs = OutdoorClimbing.all
 
-   
+    if params[:time_period] == "All Logs"
+      start_date = nil
+      end_date = nil
+    elsif params[:time_period] == "This Week"
+      start_date = Time.now.beginning_of_week
+      end_date = Time.now.end_of_week
+    elsif params[:time_period] == "This Month"
+      start_date = Time.now.beginning_of_month
+      end_date = Time.now.end_of_month
+    elsif params[:time_period] == "This Year"
+      start_date = Time.now.beginning_of_year
+      end_date = Time.now.end_of_year
+    else
+      start_date = nil
+      end_date = nil
+    end
+
+    if start_date && end_date
+      @log_climbs_filtered = @log_climbs.where(user_id: current_user.id, date: start_date..end_date)
+    else
+      @log_climbs_filtered = @log_climbs.where(user_id: current_user.id)
+    end
   end
 
 end
