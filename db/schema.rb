7fe312844ee28930fc3741afa00a03821f97ebba
SoliriusAdmin129 <soliriusadmin129@SoliriusAdmin129s-MacBook-Pro.local>
2023-11-08 18:37:49 +0000

    remove nameofuser

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_04_120531) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_08_183337) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -65,14 +65,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_04_120531) do
     t.index ["longitude"], name: "index_locations_on_longitude"
   end
 
-  create_table "nameofusers", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
-    t.index ["user_id"], name: "index_nameofusers_on_user_id"
-  end
-
   create_table "outdoor_climbings", force: :cascade do |t|
     t.date "date"
     t.string "location"
@@ -100,6 +92,5 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_04_120531) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "individual_outdoor_climbs", "outdoor_climbings"
-  add_foreign_key "nameofusers", "users"
   add_foreign_key "outdoor_climbings", "users"
 end
